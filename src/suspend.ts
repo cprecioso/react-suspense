ae8676fb8f4a9ec29937f94434fcbd634286aade
Carlos Precioso <github@precioso.design>
2023-12-09 19:16:44 +0100

    docs: document new experimentalSuspend function

--- src/suspend.ts
+++ src/suspend.ts
@@ -3,7 +3,18 @@ import { suspendOnPromise } from "./lib/suspend";
 
 const cache = new WeakMap<Promise<any>, StoredCacheValue<any>>();
 
-export const suspend = <T>(promise: Promise<T>): T =>
+/**
+ * @hidden
+ *
+ * **Experimental** This is a function that can suspend on any promise,
+ * without creating a cache first (it uses a WeakMap). Users beware,
+ * it needs testing on whether it creates reference cycles, or whether
+ * it works at all. This is hidden from the documentation for a reason.
+ *
+ * This is undocumented and thus breaking changes might happen in
+ * between minor releases.
+ */
+export const experimentalSuspend = <T>(promise: Promise<T>): T =>
   suspendOnPromise(
     () => promise,
     cache.get(promise),
